00510bafa893f0053935a41f605651c9249a5534
lipengzhou <lpzmail@163.com>
2018-11-06 17:35:28 +0800

    VuePress Config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -47,11 +47,34 @@ module.exports = {
   // 默认主题配置选项参考：https://vuepress.vuejs.org/zh/theme/default-theme-config.html
   themeConfig: {
     nav: [
-      { text: 'GitHub', link: 'https://github.com/lipengzhou/nodejs-tutorial' }
     ],
+
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: 'lipengzhou/nodejs-tutorial',
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: 'GitHub',
+
+    // 以下为可选的编辑链接选项
+
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    docsRepo: 'lipengzhou/nodejs-tutorial',
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: 'docs',
+    // 假如文档放在一个特定的分支下：
+    docsBranch: 'master',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '在 GitHub 上编辑此页',
+
     // 全局设置是否显示导航栏，也可以在页面中单独设置
     navbar: true,
-    lastUpdated: 'Last Updated',
+
+    // 最后更新时间
+    lastUpdated: '上次更新:',
+
+    // 侧边栏
     sidebar: [
       '/00-course_introduction',
       '/01-web_concept',
@@ -79,7 +102,7 @@ module.exports = {
    */
   plugins: [
     '@vuepress/back-to-top', // 回到顶部
-    '@vuepress/pagination', // 分页
+    // '@vuepress/pagination', // 分页
     '@vuepress/medium-zoom', // 图片缩放
   ],
 
